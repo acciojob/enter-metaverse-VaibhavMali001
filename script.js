3a34039fefea5810cb84089c35d16232dae90484
Vaibhav Rameshwar Mali <malivaibhav.7620@gmail.com>
2023-09-21 15:59:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-//your JS code here. If required.
+const button = document.getElementById("enterBtn");
+const heading = document.getElementById("status");
+
+button.addEventListener("click", () => {
+  heading.innerHTML = "<h1>Entered Metaverse</h1>"; // Change the text to <h2> tag
+});
